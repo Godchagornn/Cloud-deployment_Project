b58357b07dede17ac96e6ea5448fa1a168e34d62
Yasatsawin <yasatsawin_p@cmu.ac.th>
2025-10-15 11:32:35 +0700

    Link HTML, CSS

--- src/server.js
+++ src/server.js
@@ -1,3 +1,15 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import app from "./app.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../public")));
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`TMS API running on http://localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`TMS API running on http://localhost:${PORT}`);
+  console.log(`Login page available at http://localhost:${PORT}/login.html`);
+});
